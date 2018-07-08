ae29d8bf7d52b1347bcb6d6e3edddce72ee620e2
Aliska <aliska2602@yandex.ru>
2018-07-08 23:02:31 +0300

    add bug views; add custom select component.

---
+++ src/components/Bug/TableItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bug-table-item">
+        <div class="bug-table-item__description"></div>
+        <div class="bug-table-item__priority"></div>
+        <div class="bug-table-item__file"></div>
+        <div class="bug-table-item__date-created"></div>
+        <div class="bug-table-item__date-edited"></div>
+        <div class="bug-table-item__author"></div>
+        <div class="bug-table-item__assignee"></div>
+        <div class="bug-table-item__status"></div>
+        <div class="bug-table-item__comment"></div>
+        <div class="bug-table-item__actions"></div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
---
+++ src/components/common/CustomSelect.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="custom-select" :class="{'custom-select--active': is_show}" ref="custom_select" @click.self.stop="showSelect">
+        <div class="custom-select__value"></div>
+        <div class="custom-select__inner">
+            <div class="custom-select__option" :key="option.title+'_'+option.value" v-for="option in items">{{option.label}}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name : 'CustomSelect',
+    data : {
+        is_show : false,
+    },
+    props : {
+        items : Array,
+    },
+    methods : {
+        closeSelect (e) {
+            const path = e.path || (e.composedPath && e.composedPath());
+            if (e.target === this.$refs.custom_select || (path && path.indexOf(this.$refs.custom_select) > -1)) {
+
+            } else {
+                this.is_show = false;
+            }
+
+        },
+        showSelect () {
+            this.is_show = true;
+        }
+    },
+    created () {
+        document.addEventListener("click", this.closeSelect.bind(this));
+    }
+}
+</script>
+
